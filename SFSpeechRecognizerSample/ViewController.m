10ec54bbc0aab72996e8c7cb1fe755bbe049940f
marumimumemo <sagaminumber3@hotmail.co.jp>
2019-12-05 13:36:14 +0900

    SFSpeechRecognizer

--- SFSpeechRecognizerSample/ViewController.m
+++ SFSpeechRecognizerSample/ViewController.m
@@ -10,13 +10,109 @@
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UILabel *label;
+@property (weak, nonatomic) IBOutlet UIButton *button;
+
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    //locale
+    speechRecognizer = [[SFSpeechRecognizer alloc] initWithLocale:[[NSLocale alloc] initWithLocaleIdentifier:@"ja-JP"]];
+    //delegate
+    speechRecognizer.delegate = self;
+}
+
+- (void)viewDidAppear:(BOOL)animated {
+    [super viewDidLoad];
+    //authorization
+    [SFSpeechRecognizer requestAuthorization:^(SFSpeechRecognizerAuthorizationStatus status) {
+        switch (status) {
+            case SFSpeechRecognizerAuthorizationStatusAuthorized:
+                NSLog(@"Authorized");
+                break;
+            case SFSpeechRecognizerAuthorizationStatusDenied:
+                NSLog(@"音声認識へのアクセスが拒否されています。");
+                break;
+            case SFSpeechRecognizerAuthorizationStatusNotDetermined:
+                NSLog(@"音声認識はまだ許可されていません。");
+                break;
+            case SFSpeechRecognizerAuthorizationStatusRestricted:
+                NSLog(@"この端末で音声認識はできません。");
+                break;
+            default:
+                break;
+        }
+    }];
+}
+
+- (void)startRecording {
     
+    // Initialize the AVAudioEngine
+    audioEngine = [[AVAudioEngine alloc] init];
+    
+    // Make sure there's not a recognition task already running
+    if (recognitionTask) {
+        [recognitionTask cancel];
+        recognitionTask = nil;
+    }
+    
+    // Starts an AVAudio Session
+    NSError *error;
+    AVAudioSession *audioSession = [AVAudioSession sharedInstance];
+    [audioSession setCategory:AVAudioSessionCategoryRecord error:&error];
+    [audioSession setActive:YES withOptions:AVAudioSessionSetActiveOptionNotifyOthersOnDeactivation error:&error];
+    
+    // Starts a recognition process, in the block it logs the input or stops the audio
+    // process if there's an error.
+    recognitionRequest = [[SFSpeechAudioBufferRecognitionRequest alloc] init];
+    AVAudioInputNode *inputNode = audioEngine.inputNode;
+    recognitionRequest.shouldReportPartialResults = YES;
+    recognitionTask = [speechRecognizer recognitionTaskWithRequest:recognitionRequest resultHandler:^(SFSpeechRecognitionResult * _Nullable result, NSError * _Nullable error) {
+        BOOL isFinal = NO;
+        if (result) {
+            // Whatever you say in the microphone after pressing the button should be being logged
+            // in the console.
+            NSLog(@"RESULT:%@",result.bestTranscription.formattedString);
+            self.label.text = result.bestTranscription.formattedString;
+            isFinal = !result.isFinal;
+        }
+        if (error) {
+            [self->audioEngine stop];
+            [inputNode removeTapOnBus:0];
+            self->recognitionRequest = nil;
+            self->recognitionTask = nil;
+        }
+    }];
+    
+    // Sets the recording format
+    AVAudioFormat *recordingFormat = [inputNode outputFormatForBus:0];
+    [inputNode installTapOnBus:0 bufferSize:1024 format:recordingFormat block:^(AVAudioPCMBuffer * _Nonnull buffer, AVAudioTime * _Nonnull when) {
+        [self->recognitionRequest appendAudioPCMBuffer:buffer];
+    }];
+    
+    // Starts the audio engine, i.e. it starts listening.
+    [audioEngine prepare];
+    [audioEngine startAndReturnError:&error];
+    self.label.text = @"何か喋ってください";
+    NSLog(@"Say Something, I'm listening");
+}
+
+- (IBAction)microPhoneTapped:(id)sender {
+    if (audioEngine.isRunning) {
+        [audioEngine stop];
+        [recognitionRequest endAudio];
+    } else {
+        [self startRecording];
+    }
+}
+
+#pragma mark - SFSpeechRecognizerDelegate Delegate Methods
+
+- (void)speechRecognizer:(SFSpeechRecognizer *)speechRecognizer availabilityDidChange:(BOOL)available {
+    NSLog(@"Availability:%d",available);
 }
 
 

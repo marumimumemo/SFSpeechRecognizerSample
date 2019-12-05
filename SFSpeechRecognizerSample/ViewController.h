b001865850ebe77d7648c66e723f45a00c2ae37f
marumimumemo <sagaminumber3@hotmail.co.jp>
2019-12-05 12:58:13 +0900

    Import the Speech framework, assign the Delegate and declare variables

--- SFSpeechRecognizerSample/ViewController.h
+++ SFSpeechRecognizerSample/ViewController.h
@@ -7,9 +7,14 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <Speech/Speech.h>
 
-@interface ViewController : UIViewController
-
+@interface ViewController : UIViewController <SFSpeechRecognizerDelegate> {
+    SFSpeechRecognizer *speechRecognizer;
+    SFSpeechAudioBufferRecognitionRequest *recognitionRequest;
+    SFSpeechRecognitionTask *recognitionTask;
+    AVAudioEngine *audioEngine;
+}
 
 @end
 
